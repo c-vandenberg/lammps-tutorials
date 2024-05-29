08ac3b08a66406e7c8e3f1ceff44888418fec567
Chris van den Berg <cvandenberg1105@googlemail.com>
2024-05-29 20:28:17 +0100

    Update first-input notebook and generate figures

--- simon-gravelle/level-1/lennard-jones-fluid/first-input/python/src/main.py
+++ simon-gravelle/level-1/lennard-jones-fluid/first-input/python/src/main.py
@@ -74,7 +74,7 @@ def main():
         x_lim=(0, 20),
         graph_title='Lennard-Jones Fluid Potential Energy vs Time (During Energy Minimization & Molecular Dynamics '
                     'Simulation)',
-        figure_text=r'$\bf{Fig\ 2}$ Potential energy ($p_{e}$) rapidly decreases during energy minimization (orange),'
+        figure_text=r'$\bf{Fig\ 2}$ Potential energy ($p_{e}$) rapidly decreases during energy minimization (orange), '
                     r'increases once the molecular dynamics simulation starts (blue) before plateauing at ~ -0.25',
         figure_text_font_size=12,
         font_size=15,
@@ -89,13 +89,13 @@ def main():
         line_labels=['Energy Minimization', 'Molecular Dynamics'],
         line_colours=['orange', 'cyan'],
         x_label=r'$t$',
-        y_label=r'$p_{e}$',
+        y_label=r'$k_{e}$',
         y_lim=(-0.05, 2.0),
         x_lim=(0, 20),
         graph_title='Lennard-Jones Fluid Kinetic Energy vs Time (During Energy Minimization & Molecular Dynamics '
                     'Simulation)',
         figure_text=r'$\bf{Fig\ 3}$ Kinetic energy ($k_{e}$) is zero during energy minimization (orange) due to no '
-                    r'atomic or molecular motion. It increases once the molecular dynamics simulation starts (blue) '
+                    r'atomic or molecular motion. Increases once the molecular dynamics simulation starts (blue) '
                     r'before plateauing at ~ -1.5',
         figure_text_font_size=12,
         font_size=15,
