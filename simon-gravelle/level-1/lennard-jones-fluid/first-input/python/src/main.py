198839ad8a60a2588d6cb3fd84275209a1b4dafd
Chris van den Berg <cvandenberg1105@googlemail.com>
2024-06-10 20:31:32 +0100

    Refactor first-input Python and JupyterLab notebook logic

--- simon-gravelle/level-1/lennard-jones-fluid/first-input/python/src/main.py
+++ simon-gravelle/level-1/lennard-jones-fluid/first-input/python/src/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 import lammps_logfile
 import numpy
 from numpy import ndarray
@@ -7,6 +8,9 @@ from visualisation.lennard_jones_fluid_interactions_plot import LennardJonesFlui
 
 
 def main():
+    base_dir: str = os.path.dirname(__file__)
+    first_input_log_file_path: str = os.path.join(base_dir, '../../logs/first-input-log.lammps')
+
     # Define intermolecular range values and Lennard-Jones parameters
     r: ndarray = numpy.arange(0.5, 10, 0.001)
     lj_parameters: List = [
@@ -16,18 +20,17 @@ def main():
     ]
 
     # Calculate Lennard-Jones potentials
-    lennard_jones_fluid_interactions_plot: LennardJonesFluidInteractionsPlot = LennardJonesFluidInteractionsPlot()
-    lj_potentials: List[ndarray] = lennard_jones_fluid_interactions_plot.calculate_lennard_jones_potentials(
+    lj_potentials: List[ndarray] = LennardJonesFluidInteractionsPlot.calculate_lennard_jones_potentials(
         r,
         lj_parameters
     )
 
     # Plot line graph of Lennard-Jones potentials vs intermolecular range
-    lennard_jones_fluid_interactions_plot.single_line_graph(
+    LennardJonesFluidInteractionsPlot.single_line_graph(
         data_arrays=lj_potentials,
         figure_size=(18, 6),
-        line_labels=[r'$E_{12}$', r'$E_{22}$', r'$E_{11}$'],
-        line_colours=['gray', 'cyan', 'orange'],
+        line_labels=[r'$E_{11}$', r'$E_{22}$', r'$E_{12}$'],
+        line_colours=['orange', 'cyan', 'gray'],
         x_label=r'$r$',
         y_label=r'$E(r)$',
         y_lim=(-1.055, 1.055),
@@ -35,13 +38,16 @@ def main():
         graph_title=r'$\bf{Lennard-Jones\ Potential\ vs\ Interatomic\ Distance}$',
         figure_text=r'$\bf{Fig\ 1}$ Lennard-Jones potential $E_{ij}(r)$ as a function of the interatomic distance',
         figure_text_font_size=20,
+        figure_text_x_coord=0.5,
+        figure_text_y_coord=0.0005,
         font_size=20,
-        label_size=20,
-        line_width=4
+        tick_label_size=20,
+        line_width=4,
+        dashed_lines=[('y', 0)]
     )
 
     # Extract first-input-log.lammps log file data & instantiate lammps_logfile.File object
-    log_file: lammps_logfile.File = lammps_logfile.File('../../logs/first-input-log.lammps')
+    log_file: lammps_logfile.File = lammps_logfile.File(first_input_log_file_path)
     timestep: float = 0.005
     pe_vs_time: List[ndarray] = []
     ke_vs_time: List[ndarray] = []
@@ -63,7 +69,7 @@ def main():
     ke_vs_time.append(numpy.vstack((molecular_dynamics_time, molecular_dynamics_kinetic_energy)))
 
     # Plot line graph of potential energy vs time for energy minimization & molecular dynamics simulation
-    lennard_jones_fluid_interactions_plot.single_line_graph(
+    LennardJonesFluidInteractionsPlot.single_line_graph(
         data_arrays=pe_vs_time,
         figure_size=(18, 10),
         line_labels=['Energy Minimization', 'Molecular Dynamics'],
@@ -72,35 +78,37 @@ def main():
         y_label=r'$p_{e}$',
         y_lim=(-1.0, 0.0),
         x_lim=(0, 20),
-        graph_title=r'$\bf{Lennard-Jones\ Fluid\ Potential\ Energy\ vs\ Time\ (During\ Energy\ Minimization\ &\ '
-                    r'Molecular\ Dynamics\ Simulation)}$',
+        graph_title=r'$\bf{Lennard-Jones\ Fluid\ Potential\ Energy\ vs\ Time}$',
         figure_text=r'$\bf{Fig\ 2}$ Potential energy ($p_{e}$) rapidly decreases during energy minimization (orange), '
                     r'increases once the molecular dynamics simulation starts (blue) before plateauing at ~ -0.25',
         figure_text_font_size=12,
-        font_size=15,
-        label_size=20,
+        figure_text_x_coord=0.5,
+        figure_text_y_coord=0.0005,
+        font_size=25,
+        tick_label_size=20,
         line_width=3.5
     )
 
     # Plot line graph of kinetic energy vs time for energy minimization & molecular dynamics simulation
-    lennard_jones_fluid_interactions_plot.single_line_graph(
-        data_arrays=ke_vs_time,
-        figure_size=(18, 10),
-        line_labels=['Energy Minimization', 'Molecular Dynamics'],
-        line_colours=['orange', 'cyan'],
-        x_label=r'$t$',
-        y_label=r'$k_{e}$',
-        y_lim=(-0.05, 2.0),
-        x_lim=(0, 20),
-        graph_title=r'$\bf{Lennard-Jones\ Fluid\ Kinetic\ Energy\ vs\ Time\ (During\ Energy\ Minimization\ &\ '
-                    r'Molecular\ Dynamics\ Simulation)}$',
-        figure_text=r'$\bf{Fig\ 3}$ Kinetic energy ($k_{e}$) is zero during energy minimization (orange) due to no '
-                    r'atomic or molecular motion. Increases once the molecular dynamics simulation starts (blue) '
-                    r'before plateauing at ~ -1.5',
-        figure_text_font_size=12,
-        font_size=15,
-        label_size=20,
-        line_width=3.5
+    LennardJonesFluidInteractionsPlot.single_line_graph(
+       data_arrays=ke_vs_time,
+       figure_size=(18, 10),
+       line_labels=['Energy Minimization', 'Molecular Dynamics'],
+       line_colours=['orange', 'cyan'],
+       x_label=r'$t$',
+       y_label=r'$k_{e}$',
+       y_lim=(-0.05, 2.0),
+       x_lim=(0, 20),
+       graph_title=r'$\bf{Lennard-Jones\ Fluid\ Kinetic\ Energy\ vs\ Time}$',
+       figure_text=r'$\bf{Fig\ 3}$ Kinetic energy ($k_{e}$) is zero during energy minimization (orange) due to no '
+                   r'atomic or molecular motion. Increases once the molecular dynamics simulation starts (blue) '
+                   r'before plateauing at ~ -1.5',
+       figure_text_font_size=12,
+       figure_text_x_coord=0.5,
+       figure_text_y_coord=0.0005,
+       font_size=25,
+       tick_label_size=20,
+       line_width=3.5
     )
 
 
--- simon-gravelle/md-analysis-tutorial/python/src/modules/line_graph.py
+++ simon-gravelle/md-analysis-tutorial/python/src/modules/line_graph.py
@@ -6,107 +6,58 @@ from typing import List, Union, Tuple
 
 class LineGraph:
     @staticmethod
-    def line_graph_subplots(data_arrays: List[ndarray], subplot_titles: List, x_labels: List, y_labels: List,
-                            x_lim: Union[Tuple, List[Tuple]], y_lims: Union[Tuple, List[Tuple]], graph_title: str,
-                            figure_text: str):
-        """
-        Create multiple line graph subplots.
-
-        Parameters
-        ----------
-        data_arrays : List[ndarray]
-            List of NumPy data arrays where each array contains x and y data for a subplot.
-        subplot_titles : List[str]
-            List of titles for each subplot.
-        x_labels : List[str]
-            List of x-axis labels for each subplot.
-        y_labels : List[str]
-            List of y-axis labels for each subplot.
-        x_lim : Union[Tuple[float, float], List[Tuple[float, float]]]
-            Limits for the x-axis. Can be a single tuple for a single subplot or a list of tuples for multiple subplots.
-        y_lims : Union[Tuple[float, float], List[Tuple[float, float]]]
-            Limits for the y-axis. Can be a single tuple for a single subplot or a list of tuples for multiple subplots.
-        graph_title : str
-            Title for the graph.
-        figure_text : str
-            Additional text to display at the bottom of the figure.
-
-        Returns
-        -------
-        None
-        """
-        subplot_number: int = len(data_arrays)
-
-        line_graphs, subplot_axes = pyplot.subplots(subplot_number, 1, figsize=(10, 3 * subplot_number))
-
-        # If only one subplot, convert `axes` to list
-        if subplot_number == 1:
-            subplot_axes: List = [subplot_axes]
-
-        line_graphs.patch.set_facecolor('black')
-
-        # Iterate through all subplots, stylise and plot data
-        for key, axes in enumerate(subplot_axes):
-            # Stylise subplot
-            axes.set_facecolor('black')
-            axes.tick_params(colors='white', which='both')
-            for spine in axes.spines.values():
-                spine.set_edgecolor('white')
-
-            # Plot data
-            axes.plot(data_arrays[key][:, 0], data_arrays[key][:, 1], color='cyan')
-            axes.set_title(subplot_titles[key], color='white')
-            axes.set_xlabel(x_labels[key], color='white')
-            axes.set_ylabel(y_labels[key], color='white')
-            axes.set_ylim(y_lims[key])
-            axes.set_xlim(x_lim)
-
-        pyplot.subplots_adjust(hspace=0.5)
-
-        line_graphs.suptitle(graph_title, color='white')
-        line_graphs.text(0.5, 0.0005, figure_text, ha='center', va='center', color='white', fontsize=12)
-
-        pyplot.tight_layout()
-        pyplot.show()
-
-    @staticmethod
     def single_line_graph(data_arrays: List[ndarray], figure_size: Tuple[int, int],
-                          line_colours: List, x_label: str, y_label: str, y_lim: Tuple, x_lim: Tuple, graph_title: str,
-                          figure_text: str, figure_text_font_size: int, font_size: int, label_size: int, line_width: int,
-                          line_labels: Union[List, None] = None):
+                          line_colours: List, x_label: str, y_label: str, x_lim: Tuple, y_lim: Tuple, graph_title: str,
+                          figure_text: str, figure_text_font_size: Union[int, float],
+                          figure_text_x_coord: Union[int, float], figure_text_y_coord: Union[int, float],
+                          font_size: Union[int, float], tick_label_size: Union[int, float],
+                          line_width: Union[int, float], save_path: Union[str, None] = None,
+                          line_labels: Union[List, None] = None,
+                          line_labels_position: str = None,
+                          dashed_lines: List[Tuple[str, Union[int, float]]] = None):
         """
-        Create a single line graph with multiple lines.
+        Create a single line graph with single or multiple lines.
 
         Parameters
         ----------
         data_arrays : List[ndarray]
-            List of NumPy data arrays where each array contains x and y data for a line plot.
+            List of NumPy data arrays where each array contains x and y data.
         figure_size : Tuple[int, int]
-            Size of the figure (width, height)
+            Size of the figure (width, height).
         line_colours : List[str]
             List of colors for each line.
         x_label : str
             Label for the x-axis.
         y_label : str
             Label for the y-axis.
-        y_lim : Tuple[float, float]
-            Limits for the y-axis.
         x_lim : Tuple[float, float]
             Limits for the x-axis.
+        y_lim : Tuple[float, float]
+            Limits for the y-axis.
         graph_title : str
             Title for the graph.
         figure_text : str
             Additional text to display at the bottom of the figure.
         figure_text_font_size: int
             Font size for the text displayed at the bottom of the figure.
-        font_size : int
+        figure_text_x_coord : Union[int, float]
+            Figure text x-coordinate.
+        figure_text_y_coord : Union[int, float]
+            Figure text y-coordinate.
+        font_size : Union[int, float]
             Font size for the axis labels, tick labels and title.
-        label_size : int
+        tick_label_size : Union[int, float]
             Font size for the tick labels.
-        line_width : int
+        line_width : Union[int, float]
             Width of the lines in the plot.
+        save_path : Union[str, None]
+            Directory path where the plot will be saved (optional).
         line_labels : Union[List, None]
             List of labels for each line (optional).
+        line_labels_position : str
+            Position for the line labels (optional).
+        dashed_lines : List[Tuple[str, Union[int, float]]]
+            Dashed lines for the x-axis and/or y-axis (optional)
 
         Returns
         -------
@@ -118,7 +69,7 @@ class LineGraph:
         line_graph_axes.set_facecolor('black')
 
         # Stylise ticks and spines
-        line_graph_axes.tick_params(colors='white', which='both', labelsize=label_size)
+        line_graph_axes.tick_params(colors='white', which='both', labelsize=tick_label_size)
         for spine in line_graph_axes.spines.values():
             spine.set_edgecolor('white')
 
@@ -139,17 +90,30 @@ class LineGraph:
         line_graph_axes.set_xlim(x_lim)
         line_graph_axes.set_title(graph_title, fontsize=font_size, color='white')
 
-        # Add legend
-        legend: Legend = line_graph_axes.legend(loc='center left', bbox_to_anchor=(1, 0.5), frameon=False,
-                                                fontsize=font_size)
-        for text in legend.get_texts():
-            text.set_color('white')
+        if dashed_lines:
+            for axis, coord in dashed_lines:
+                if axis.lower() not in ['x', 'y']:
+                    raise ValueError('Dashed line axis must be "x" or "y"')
 
-        # Add figure title
-        line_graph.text(0.5, 0.0005,
-                        figure_text,
-                        ha='center', va='center', color='white', fontsize=figure_text_font_size)
+                if axis.lower() == 'x':
+                    line_graph_axes.axvline(coord, color='white', linestyle='--', linewidth=1.5)
+                else:
+                    line_graph_axes.axhline(coord, color='white', linestyle='--', linewidth=1.5)
 
-        # Show plot
-        pyplot.tight_layout()
+        if line_labels:
+            label_position = line_labels_position if line_labels_position is not None else 'upper right'
+
+            # Add legend
+            legend: Legend = line_graph_axes.legend(loc=label_position, frameon=False,
+                                                    fontsize=font_size)
+            for text in legend.get_texts():
+                text.set_color('white')
+
+        # Add figure text
+        line_graph.text(figure_text_x_coord, figure_text_y_coord, figure_text, ha='center', va='center',
+                        color='white', fontsize=figure_text_font_size)
+
+        if save_path:
+            pyplot.savefig(save_path, bbox_inches='tight')
+
         pyplot.show()
